6590516060c46fce32090a8003031012fb49c274
Keenan Graham <ksgraham@stanford.edu>
2023-02-23 11:52:28 -0800

    Add reindexing behvaior control

--- src/snovault/elasticsearch/manage_mappings.py
+++ src/snovault/elasticsearch/manage_mappings.py
@@ -1,8 +1,14 @@
 from pyramid.paster import get_app
 
+from pyramid.router import Router
+
 from webtest import TestApp
 
+from opensearchpy import OpenSearch
+
 from opensearchpy.exceptions import NotFoundError
+
+from dataclasses import dataclass
 
 from .create_mapping import generate_indices_and_mappings
 from .create_mapping import index_settings as get_index_settings
@@ -10,6 +16,20 @@ from .create_mapping import create_and_set_index_mapping
 
 from .interfaces import ELASTIC_SEARCH
 from .interfaces import RESOURCES_INDEX as ALL_RESOURCES_ALIAS
+
+from typing import Any
+from typing import Dict
+from typing import Literal
+
+
+@dataclass
+class ManageMappingsProps:
+    app: Router
+    opensearch_client: OpenSearch
+    type_alias_to_current_index_name: Dict[str, Any]
+    mappings: Dict[str, Any]
+    all_resources_alias: str
+    should_reindex: Literal['always', 'never', 'after-initial']
 
 
 def reindex_by_collection(app, collection):
@@ -27,22 +47,25 @@ def reindex_by_collection(app, collection):
     )
 
 
-def get_aliases(type_alias, all_resources_alias=ALL_RESOURCES_ALIAS):
+def get_aliases(props: ManageMappingsProps, type_alias):
     return {
         type_alias: {},
-        all_resources_alias: {},
+        props.all_resources_alias: {},
     }
 
 
-def create_index(opensearch_client, type_alias, current_index_name, mappings):
+def create_index(props: ManageMappingsProps, type_alias, current_index_name):
     print(f'Creating index {current_index_name} for type {type_alias}')
     index_settings = get_index_settings()
-    index_settings['aliases'] = get_aliases(type_alias)
+    index_settings['aliases'] = get_aliases(
+        props,
+        type_alias
+    )
     create_and_set_index_mapping(
-        es=opensearch_client,
+        es=props.opensearch_client,
         index=current_index_name,
         index_settings=index_settings,
-        mapping=mappings[type_alias],
+        mapping=props.mappings[type_alias],
     )
 
 
@@ -55,16 +78,16 @@ def reindex_collections(app, collections):
         )
 
 
-def clean_up_auto_created_indices(opensearch_client, type_alias_to_current_index_name, all_resources_alias=ALL_RESOURCES_ALIAS):
+def clean_up_auto_created_indices(props: ManageMappingsProps):
     # We can't turn off auto_create_index at cluster level so we'll clean up manually
     # if any of our workers start writing to an index that doesn't exist yet.
     current_index_names = get_current_index_names(
-        type_alias_to_current_index_name
+        props.type_alias_to_current_index_name
     )
     try:
         all_existing_resources_indices = list(
-            opensearch_client.indices.get_alias(
-                all_resources_alias
+            props.opensearch_client.indices.get_alias(
+                props.all_resources_alias
             ).keys()
         )
     except NotFoundError as e:
@@ -75,26 +98,39 @@ def clean_up_auto_created_indices(opensearch_client, type_alias_to_current_index
         if current_index_name in all_existing_resources_indices:
             # It's aliased so we created it.
             continue
-        if opensearch_client.indices.exists(current_index_name):
+        if props.opensearch_client.indices.exists(current_index_name):
             # It exists but we didn't create it.
             print('Deleting unaliased (autocreated?) index', current_index_name)
-            print(opensearch_client.indices.delete(current_index_name))
+            print(props.opensearch_client.indices.delete(current_index_name))
 
 
-def create_latest_indices_and_reindex(app, opensearch_client, type_alias_to_current_index_name, mappings):
+def should_reindex_collection(props: ManageMappingsProps, type_alias):
+    if props.should_reindex == 'never':
+        return False
+    elif props.should_reindex == 'always':
+        return True
+    elif props.should_reindex == 'after-initial':
+        try:
+            return len(props.opensearch_client.indices.get_alias(type_alias)) > 0
+        except Exception:
+            return False
+
+
+def create_latest_indices_and_reindex(props: ManageMappingsProps):
     collections_to_reindex = []
-    for type_alias, current_index_name in type_alias_to_current_index_name.items():
-        if not opensearch_client.indices.exists(current_index_name):
+    for type_alias, current_index_name in props.type_alias_to_current_index_name.items():
+        if not props.opensearch_client.indices.exists(current_index_name):
+            # Check before creating new index.
+            if should_reindex_collection(props, type_alias):
+                collections_to_reindex.append(type_alias)
             create_index(
-                opensearch_client=opensearch_client,
+                props=props,
                 type_alias=type_alias,
                 current_index_name=current_index_name,
-                mappings=mappings,
             )
-            collections_to_reindex.append(type_alias)
         else:
             print(f'Index {current_index_name} for {type_alias} already exists')
-    reindex_collections(app, collections_to_reindex)
+    reindex_collections(props.app, collections_to_reindex)
 
 
 def get_current_index_names(type_alias_to_current_index_name):
@@ -104,42 +140,31 @@ def get_current_index_names(type_alias_to_current_index_name):
     ]
 
 
-def delete_old_indices_if_empty(opensearch_client, type_alias_to_current_index_name, all_resources_alias=ALL_RESOURCES_ALIAS):
+def delete_old_indices_if_empty(props: ManageMappingsProps):
     current_index_names = get_current_index_names(
-        type_alias_to_current_index_name
+        props.type_alias_to_current_index_name
     )
     all_existing_resources_indices = list(
-        opensearch_client.indices.get_alias(
-            all_resources_alias
+        props.opensearch_client.indices.get_alias(
+            props.all_resources_alias
         ).keys()
     )
     for existing_index in all_existing_resources_indices:
         if existing_index in current_index_names:
             print(f'Not deleting current index {existing_index}')
             continue
-        documents_in_index = opensearch_client.count(index=existing_index)['count']
+        documents_in_index = props.opensearch_client.count(index=existing_index)['count']
         if documents_in_index != 0:
             print(f'{existing_index} is not latest but still has documents, skipping delete')
             continue
         print(f'Deleting {existing_index}')
-        print(opensearch_client.indices.delete(index=existing_index, ignore=[400, 404]))
+        print(props.opensearch_client.indices.delete(index=existing_index, ignore=[400, 404]))
 
 
-def update(app, opensearch_client, type_alias_to_current_index_name, mappings):
-    clean_up_auto_created_indices(
-        opensearch_client=opensearch_client,
-        type_alias_to_current_index_name=type_alias_to_current_index_name,
-    )
-    create_latest_indices_and_reindex(
-        app=app,
-        opensearch_client=opensearch_client,
-        type_alias_to_current_index_name=type_alias_to_current_index_name,
-        mappings=mappings,
-    )
-    delete_old_indices_if_empty(
-        opensearch_client=opensearch_client,
-        type_alias_to_current_index_name=type_alias_to_current_index_name,
-    )
+def update(props: ManageMappingsProps):
+    clean_up_auto_created_indices(props)
+    create_latest_indices_and_reindex(props)
+    delete_old_indices_if_empty(props)
 
 
 def make_type_alias_to_current_index_name(app):
@@ -155,19 +180,16 @@ def get_mappings(app):
     return mappings
 
 
-def manage_mappings(app):
-    opensearch_client = app.registry[ELASTIC_SEARCH]
-    type_alias_to_current_index_name = make_type_alias_to_current_index_name(
-        app
+def manage_mappings(app, should_reindex='never'):
+    props = ManageMappingsProps(
+        app=app,
+        opensearch_client=app.registry[ELASTIC_SEARCH],
+        type_alias_to_current_index_name=make_type_alias_to_current_index_name(app),
+        mappings=get_mappings(app),
+        all_resources_alias=ALL_RESOURCES_ALIAS,
+        should_reindex=should_reindex
     )
-    mappings = get_mappings(app)
-    opensearch_client = app.registry[ELASTIC_SEARCH]
-    update(
-        app,
-        opensearch_client,
-        type_alias_to_current_index_name,
-        mappings,
-    )
+    update(props)
 
 
 def main():
@@ -183,12 +205,22 @@ def main():
         'config_uri',
         help='path to configfile'
     )
+    parser.add_argument(
+        '--should-reindex',
+        help='Controls reindexing behavior after creating new index',
+        choices=[
+            'always',
+            'never',
+            'after-initial',
+        ],
+        default='always',
+    )
     args = parser.parse_args()
     app = get_app(
         args.config_uri,
         args.app_name
     )
-    manage_mappings(app)
+    manage_mappings(app, args.should_reindex)
 
 
 if __name__ == '__main__':
