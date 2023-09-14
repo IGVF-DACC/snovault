ee0b1a6006a1f2bd2e2136e34d2673257ee7eb27
Keenan Graham <ksgraham@stanford.edu>
2023-09-14 13:54:29 -0700

    Update

--- src/snovault/feature_flags.py
+++ src/snovault/feature_flags.py
@@ -10,16 +10,25 @@ FEATURE_FLAGS = 'FEATURE_FLAGS'
 
 def includeme(config):
     registry = config.registry
-    registry[FEATURE_FLAGS] = register_feature_flags(registry.settings)
+    registry[FEATURE_FLAGS] = initialize_feature_flags(registry.settings)
     config.add_route('feature_flags', '/feature-flags{slash:/?}')
     config.scan(__name__, categories=None)
 
 
+def get_local_feature_flags():
+    return {
+        'block_database_writes': {
+            'enabled': False
+        }
+    }
 
-def register_feature_flags(settings):
+
+def initialize_feature_flags(settings):
     feature_flag_strategy = settings.get('feature_flag_strategy')
     if feature_flag_strategy == 'local':
-        return LocalFeatureFlags()
+        return LocalFeatureFlags(
+            feature_flags=get_local_feature_flags()
+        )
     elif feature_flag_strategy == 'appconfig':
         return AppConfigHelper(
             appconfig_application=os.environ.get('APPCONFIG_APPLICATION'),
@@ -32,16 +41,12 @@ def register_feature_flags(settings):
 
 class LocalFeatureFlags:
 
-    def __init__(self):
-        self.config = {
-            'block_database_writes': {
-                'enabled': False
-            }
-        }
+    def __init__(self, feature_flags):
+        self._config = feature_flags
 
     @property
     def config(self):
-        return self.config
+        return self._config
 
 
 @view_config(
