83e7d0cebaba5080b4fb387773897d1d958b2ba3
Keenan Graham <ksgraham@stanford.edu>
2023-10-30 12:25:17 -0700

    Update

--- src/snowflakes/tests/indexing/test_indexing.py
+++ src/snowflakes/tests/indexing/test_indexing.py
@@ -41,54 +41,54 @@ def test_indexing_opensearch_mappings_exist(testapp, registry, dummy_request, wo
     os = registry[ELASTIC_SEARCH]
     actual = list(sorted(os.indices.get('*').keys()))
     expected = list(sorted([
-        'access_key_828db54a',
-        'award_0928c743',
-        'image_71f1370f',
-        'lab_97f7fb39',
-        'page_9e9f92d5',
-        'snowball_c99d8a41',
-        'snowflake_8d771cc3',
-        'snowfort_2eaa2355',
-        'testing_bad_accession_f37ff11d',
-        'testing_custom_embed_source_562e6c63',
-        'testing_custom_embed_target_ebd058c8',
-        'testing_dependencies_d4c0e10a',
-        'testing_download_9e40e0b4',
-        'testing_link_source_5a382472',
-        'testing_link_target_ed04b07b',
-        'testing_post_put_patch_9f695bfd',
-        'testing_search_schema_8e208318',
-        'testing_search_schema_special_facets_98447bdb',
-        'testing_server_default_9b7e7a5c',
-        'user_7a155f0f'
+        'access_key_5bd72e25',
+        'award_8ab12598',
+        'image_9d79d5e4',
+        'lab_dd2e527f',
+        'page_54c9ebeb',
+        'snowball_6f4c73a4',
+        'snowflake_30db45cf',
+        'snowfort_5e0163f6',
+        'testing_bad_accession_68bd87b2',
+        'testing_custom_embed_source_d90ac323',
+        'testing_custom_embed_target_6881adca',
+        'testing_dependencies_965e4e95',
+        'testing_download_ff92c2cb',
+        'testing_link_source_de2a5001',
+        'testing_link_target_1b833428',
+        'testing_post_put_patch_a469c52f',
+        'testing_search_schema_cac5d36a',
+        'testing_search_schema_special_facets_ce73ae5f',
+        'testing_server_default_fe00209c',
+        'user_5466c376'
     ]))
     assert actual == expected, actual
     actual = list(os.indices.get_alias('award').keys())
     expected = [
-        'award_0928c743',
+        'award_8ab12598'
     ]
     assert actual == expected, actual
     actual = list(sorted(os.indices.get_alias('snovault-resources').keys()))
     expected = list(sorted([
-        'access_key_828db54a',
-        'award_0928c743',
-        'image_71f1370f',
-        'lab_97f7fb39',
-        'page_9e9f92d5',
-        'snowball_c99d8a41',
-        'snowflake_8d771cc3',
-        'snowfort_2eaa2355',
-        'testing_bad_accession_f37ff11d',
-        'testing_custom_embed_source_562e6c63',
-        'testing_custom_embed_target_ebd058c8',
-        'testing_dependencies_d4c0e10a',
-        'testing_download_9e40e0b4',
-        'testing_link_source_5a382472',
-        'testing_link_target_ed04b07b',
-        'testing_post_put_patch_9f695bfd',
-        'testing_search_schema_8e208318',
-        'testing_search_schema_special_facets_98447bdb',
-        'testing_server_default_9b7e7a5c',
-        'user_7a155f0f'
+        'access_key_5bd72e25',
+        'award_8ab12598',
+        'image_9d79d5e4',
+        'lab_dd2e527f',
+        'page_54c9ebeb',
+        'snowball_6f4c73a4',
+        'snowflake_30db45cf',
+        'snowfort_5e0163f6',
+        'testing_bad_accession_68bd87b2',
+        'testing_custom_embed_source_d90ac323',
+        'testing_custom_embed_target_6881adca',
+        'testing_dependencies_965e4e95',
+        'testing_download_ff92c2cb',
+        'testing_link_source_de2a5001',
+        'testing_link_target_1b833428',
+        'testing_post_put_patch_a469c52f',
+        'testing_search_schema_cac5d36a',
+        'testing_search_schema_special_facets_ce73ae5f',
+        'testing_server_default_fe00209c',
+        'user_5466c376'
     ]))
     assert actual == expected, actual
