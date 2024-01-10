8507b24d5429af9f3b620a660fbc5b9e0eb769bd
Keenan Graham <ksgraham@stanford.edu>
2024-01-10 12:32:42 -0800

    Echo DLQ URL

--- docker/localstack/create_localstack.sh
+++ docker/localstack/create_localstack.sh
@@ -5,12 +5,12 @@ echo 'Creating DLQ'
 TRANSACTION_DLQ_URL=$(awslocal sqs create-queue --queue-name transaction-dead-letter-queue --query 'QueueUrl' --output text)
 TRANSACTION_DLQ_ARN=$(awslocal sqs get-queue-attributes --queue-url $TRANSACTION_DLQ_URL --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
 
-echo $TRANSACTION_DLQ_ARN
+echo $TRANSACTION_DLQ_URL
 
 INVALIDATION_DLQ_URL=$(awslocal sqs create-queue --queue-name invalidation-dead-letter-queue --query 'QueueUrl' --output text)
 INVALIDATION_DLQ_ARN=$(awslocal sqs get-queue-attributes --queue-url $INVALIDATION_DLQ_URL --attribute-names QueueArn --query 'Attributes.QueueArn' --output text)
 
-echo $INVALIDATION_DLQ_ARN
+echo $INVALIDATION_DLQ_URL
 
 echo 'Creating SQS'
 
