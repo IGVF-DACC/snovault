2d21562eddc0f8e4bcc01ded61b08d92afb3bd19
Keenan Graham <ksgraham@stanford.edu>
2024-05-06 12:04:20 -0700

    Add sleep

--- src/snowflakes/tests/test_batch_upgrade.py
+++ src/snowflakes/tests/test_batch_upgrade.py
@@ -70,6 +70,7 @@ def test_batch_upgrade_regex(testapp, invalid_award):
 
 
 def test_batch_upgrade_records_transaction(testapp, registry, award, lab):
+    import time
     item = {
         'award': award['uuid'],
         'lab': lab['uuid'],
@@ -98,6 +99,7 @@ def test_batch_upgrade_records_transaction(testapp, registry, award, lab):
         False,  # errors
         '',  # error message
     ]
+    time.sleep(3) # Wait for record to show up.
     assert registry['TRANSACTION_QUEUE'].info(
     )['ApproximateNumberOfMessages'] == '1', 'Upgrade not recorded as transaction'
     response4 = testapp.post_json(
@@ -108,5 +110,6 @@ def test_batch_upgrade_records_transaction(testapp, registry, award, lab):
             ]
         }
     ).json
+    time.sleep(3) # Make sure record doesn't show up.
     assert registry['TRANSACTION_QUEUE'].info(
     )['ApproximateNumberOfMessages'] == '1', 'Null upgrade produced transaction'
